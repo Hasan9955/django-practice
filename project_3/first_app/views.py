97bd0a34b99434310b8c453e2b8c7502a37641d1
Hasan Bin Ali <hasanbinali7556@gmail.com>
2026-07-30 22:10:23 +0600

    app home html updated

--- project_3/first_app/views.py
+++ project_3/first_app/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
+import datetime
+
 
 # Create your views here.
 def home(request):
-    d = {'author': 'Django', 'age': 20, 'list': [1, 2, 3]}
+    d = {'author': 'Django', 'age': 20, 'list': [1, 2, 3], 'courses': [
+        {
+            'id': 1,
+            'name': 'Python',
+            'price': 100
+        },
+        {
+            'id': 2,
+            'name': 'Django',
+            'price': 200
+        },
+        {
+            'id': 3,
+            'name': 'Flask',
+            'price': 300
+        }
+    ], 
+
+    'new_list': ["Python", "is", "best", "programming", "language"],
+
+    'birthdate': datetime.datetime.now(),
+    'val': "hasan bin ali"
+
+    }
+
     return render(request, 'first_app/home.html', context=d)
