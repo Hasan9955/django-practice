187c4ec1d716cad9b2bf848f7c522cdbbeeafabb
Hasan Bin Ali <hasanbinali7556@gmail.com>
2026-08-03 22:39:18 +0600

    python form api

--- practice_project_2/first_app/forms.py
+++ practice_project_2/first_app/forms.py
@@ -3,4 +3,13 @@ from django import forms
 class contactForm(forms.Form):
     name = forms.CharField(label="Username")
     email = forms.EmailField(label="Email")
-    
+    age = forms.IntegerField(label="Age")
+    weight = forms.FloatField(label="Weight")
+    balance = forms.DecimalField(label="Balance")
+    check = forms.BooleanField(label="Check")
+    birthday = forms.DateField(label="Birthday")
+    appointment = forms.DateTimeField(label="Appointment")
+    CHOICES = [("S", "Small"), ("M", "Medium"), ("L", "Large")]
+    size = forms.ChoiceField(label="Size", choices=CHOICES)
+    meal = [("p", 'Pepperoni'), ('c', 'Cheese'), ('m', 'Mushroom')]
+    pizza = forms.MultipleChoiceField(label="Pizza", choices=meal, widget=forms.CheckboxSelectMultiple)
