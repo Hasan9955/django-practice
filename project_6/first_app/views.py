306eadc1947988e7e38ba4d83141c38a8fd79cbb
Hasan9955 <hasanbinali7556@gmail.com>
2026-09-04 09:50:43 +0600

    django project

---
+++ project_6/first_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# def index(request):
+#     return render(request, 'first_app/index.html')
+
+
+# def about(request, id):
+#     return render(request, 'first_app/about.html', {'id' : id})
+
+
+# def contact(request):
+#     print(request.GET)
+#     return render(request, 'first_app/contact.html', {'id' : request.GET})
